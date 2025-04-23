10175cf5754542bf38a22af2d0d7388e4808e3b6
danirando <danielerando@tiscali.it>
2025-04-23 13:52:44 +0200

    rimosso console log

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ const refreshButton = document.querySelector(".btn");
 const generateRandomEmail = () => {
   axios.get(randomEmail).then((response) => {
     const email = response.data.response;
-    console.log(email);
     listEl.innerHTML += `<li class="list-group-item list-group-item-success">${email}<span class="badge bg-primary rounded-pill ms-3">New</span></li>`;
   });
 };
